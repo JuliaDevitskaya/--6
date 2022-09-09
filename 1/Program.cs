f0ea343aa6df21731cef1e521a9f9994d8c801e7
Julia <yagubetslulka@yandex.ru>
2022-09-09 15:04:06 +0300

    Задача1. Поиск чисел, которые больше 0

---
+++ 1/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+
+using static System.Console;
+Clear();
+
+int[] array = new int[5];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = int.Parse(ReadLine());
+}
+WriteLine($"[{String.Join(",", array)}]");
+
+int result = Calculate(array);
+WriteLine(result);
+
+
+
+int Calculate(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+
+
+
+
+
+
+
